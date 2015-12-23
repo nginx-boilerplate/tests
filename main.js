541b4731b6e14a4c7015b1b2ed30bd9eb384d5e1
Michael Gorianskyi <michael.gorianskyi@westwing.de>
2015-12-23 13:58:23 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+#!/bin/node
+var request = require('supertest');
+
+requestNormal = request('http://nginx.boilerplate/');
+requestWww = request('http://www.nginx.boilerplate/');
+
+function handle(err) {
+    if (err) {
+        console.log(err);
+    }
+}
+
+requestWww.get('').expect(301, handle);
+requestNormal.get('.git').expect(403, handle);
+requestNormal.get('').expect(200, handle);
+requestNormal.get('some/random/url/').expect(200, handle);
+requestNormal.get('dir/script.php').expect(200, 'ok', handle);
