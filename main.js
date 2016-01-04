f6538e2fa5f1daee52c7cc307fb2e35899638246
Michael Gorianskyi <michael.gorianskyi@westwing.de>
2016-01-04 22:30:25 +0100

    Added test cases

--- main.js
+++ main.js
@@ -15,3 +15,12 @@ requestNormal.get('.git').expect(403, handle);
 requestNormal.get('').expect(200, handle);
 requestNormal.get('some/random/url/').expect(200, handle);
 requestNormal.get('dir/script.php').expect(200, 'ok', handle);
+requestNormal.get('test.dir/').expect(200, "ok\n", handle);
+requestNormal.get('test.dir/index.html').expect(200, "ok\n", handle);
+requestNormal.get('/query-string-test/?query=string').expect(200, function (err, res) {
+    var match = res.text.match(/\[QUERY_STRING\] => query=string/g);
+    if (match === null) {
+        console.error('Query parameters were not transferred to Php');
+        return false;
+    }
+});
